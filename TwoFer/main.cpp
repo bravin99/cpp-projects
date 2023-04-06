8117f901cb7eb2bf55baa001005b1d70bad2e0b3
Bravin Shumwe <bravinshumwe.coding@gmail.com>
2023-04-06 11:40:03 +0300

    TwoFer, exercism exercise

---
+++ TwoFer/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+string two_fer(string Name)
+{
+    string result;
+
+    if (Name == "")
+        result = "One for you, one for me.";
+    else
+        result = "One for "+Name+", one for me.";
+
+    return result;
+}
+
+int main(int argc, char **argv) {
+    string n = "";
+    cout<<"Customer name (Press enter for no name): ";
+    getline(cin,n);
+
+    cout<<two_fer(n)<<endl;
+
+
+    return 0;
+}
