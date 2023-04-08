88f51e6eb1943374829466e9dc9508345e1c4210
Bravin Shumwe <bravinshumwe.coding@gmail.com>
2023-04-08 23:09:32 +0300

    Exercism: Hamming exercise

--- Hamming/main.cpp
+++ Hamming/main.cpp
@@ -1,7 +1,16 @@
-#include "hamming.h"
 #include <iostream>
+#include <string>
+#include <stdexcept>
+#include <numeric>
+#include <functional>
 
-using namespace hamming;
+//using namespace std;
+ int compute(const std::string & s1, const std::string & s2)
+{
+    if (s1.size() != s2.size())
+        throw std::domain_error("Size mismatch");
+    return std::inner_product(begin(s1), end(s1), begin(s2), 0, std::plus<int>{}, std::not_equal_to<int>{});
+}
 
 int main() {
     std::string s1 = "GAGCCTACTAACGGGAT";
