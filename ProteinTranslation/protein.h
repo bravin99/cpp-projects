ca0642e906611d0967346d03cff242ed63dec729
Bravin Shumwe <bravinshumwe.coding@gmail.com>
2023-04-25 17:40:00 +0300

    Protein Translation

---
+++ ProteinTranslation/protein.h
@@ -0,0 +1,12 @@
+#ifndef PROTEIN_H_INCLUDED
+#define PROTEIN_H_INCLUDED
+
+#include <string>
+#include <vector>
+
+namespace proteins
+{
+    std::vector<std::string> translate(std::string rna);
+}
+
+#endif // PROTEIN_H_INCLUDED
