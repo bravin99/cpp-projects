a45a10626961505995161e3d06cf5ee4b2cce263
Bravin Shumwe <bravinshumwe.coding@gmail.com>
2023-05-06 19:55:03 +0300

    output update

--- trinary/main.cpp
+++ trinary/main.cpp
@@ -4,10 +4,11 @@
 
 
 int main() {
-    std::string tr = "201";
+    std::string tr = "012";
     int decimal_value = trinary::to_decimal(tr);
 
-    std::cout << decimal_value << std::endl;
+    std::cout << "Trinary string: " << tr << std::endl;
+    std::cout << "Result (Decimal): " << decimal_value << std::endl;
 
     return 0;
 }
